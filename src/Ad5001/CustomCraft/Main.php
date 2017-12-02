5186d45225b9223a2c3e4f71d7eaf28c61ec04ca
Ad5001 <mail@ad5001.eu>
2017-12-03 00:57:01 +0100

    Quick fix #6

--- src/Ad5001/CustomCraft/Main.php
+++ src/Ad5001/CustomCraft/Main.php
@@ -12,16 +12,17 @@ class Main extends PluginBase{
    public function onEnable(){
         foreach($this->getConfig()->getAll() as $craft) {
             $result = $this->getItem($craft["result"]);
-            $rec = new ShapedRecipe($result, 3, 3);
-            $rec->addIngredient(0, 0, $this->getItem($craft["shape"][0][0]));
-            $rec->addIngredient(1, 0, $this->getItem($craft["shape"][0][1]));
-            $rec->addIngredient(2, 0, $this->getItem($craft["shape"][0][2]));
-            $rec->addIngredient(0, 1, $this->getItem($craft["shape"][1][0]));
-            $rec->addIngredient(1, 1, $this->getItem($craft["shape"][1][1]));
-            $rec->addIngredient(2, 1, $this->getItem($craft["shape"][1][2]));
-            $rec->addIngredient(0, 2, $this->getItem($craft["shape"][2][0]));
-            $rec->addIngredient(1, 2, $this->getItem($craft["shape"][2][1]));
-            $rec->addIngredient(2, 2, $this->getItem($craft["shape"][2][2]));
+            $rec = new ShapedRecipe($result, ["abc","def","ghi"], [
+		    "a" => $this->getItem($craft["shape"][0][0]),
+		    "b" => $this->getItem($craft["shape"][0][1]),
+		    "c" => $this->getItem($craft["shape"][0][2]),
+		    "d" => $this->getItem($craft["shape"][1][0]),
+		    "e" => $this->getItem($craft["shape"][1][1]),
+		    "f" => $this->getItem($craft["shape"][1][2]),
+		    "g" => $this->getItem($craft["shape"][2][0]),
+		    "h" => $this->getItem($craft["shape"][2][1]),
+		    "i" => $this->getItem($craft["shape"][2][2]),
+	    ]);
             $this->getServer()->getCraftingManager()->registerRecipe($rec);
             $this->getLogger()->info("Registered recipe for " . $this->getItem($craft["result"])->getName());
         }
